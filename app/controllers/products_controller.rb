b3c8d11e2f37429ce761fc83cd3751b6c437c448
Yevhen V <wik@bdhr.co>
2015-04-18 23:28:03 +0300

    Add dropbox dropins integration

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :urls, :price, :email)
+      params.require(:product).permit(:title, :price, :email, :urls => [])
     end
 end
