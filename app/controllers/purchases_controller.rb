e9d1ac3841c62330594d247f4db2ed2be2cef784
Yevhen V <wik@bdhr.co>
2015-04-19 12:36:51 +0300

    Add token based URLs for purchases

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -61,7 +61,7 @@ class PurchasesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_purchase
-      @purchase = Purchase.find(params[:id])
+      @purchase = Purchase.find_by(token: params[:id])
     end
 
     def set_product
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,8 @@
 class Purchase < ActiveRecord::Base
   belongs_to :product
+  has_secure_token :token
+
+  def to_param
+    token
+  end
 end
